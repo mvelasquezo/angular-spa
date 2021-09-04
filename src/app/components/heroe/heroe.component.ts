bcfc88d40ebf11ba77d39023fa995f59f4696065
Moisés Velásquez <1924821300101@ingenieria.usac.edu.gt>
2021-09-04 15:14:32 -0600

    agregando dist

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HeroesService } from '../../servicios/heroes.service';
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html'
 })
 export class HeroeComponent {
-  heroe = {};
+  heroe: Heroe;
   constructor( private activatedRoute: ActivatedRoute
               , private _heroesService: HeroesService ) {
     this.activatedRoute.params.subscribe( params => {
       this.heroe = this._heroesService.getHeroe( params['id'] );
     });
   }
-
-
 }
